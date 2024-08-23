551be4001b1594d1b99602223468e75b23d6d13f
Dorian Marié <dorian@dorianmarie.com>
2024-08-23 22:52:51 +0200

    add argument parsing and json mapping logic

--- spec/json_map_spec.rb
+++ spec/json_map_spec.rb
@@ -2,5 +2,14 @@
 
 require "spec_helper"
 
-RSpec.describe "each" do
+RSpec.describe "json-each" do
+  it "works" do
+    expect(`echo "[1, 2, 3]" | bin/json-map "it = it + 1"`).to eq(<<~OUTPUT)
+      [
+        2,
+        3,
+        4
+      ]
+    OUTPUT
+  end
 end
